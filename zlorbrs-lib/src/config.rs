6c9fd2fe500a67eb2b27f85674d6ba1262f0d23c
Triston Armstrong <tarmstrong95@proton.me>
2025-10-12 22:24:41 -0600

    add some config gen logic to config fns

--- zlorbrs-lib/src/config.rs
+++ zlorbrs-lib/src/config.rs
@@ -24,12 +24,45 @@ impl Config {
     }
     pub fn load(repo_name: String) -> Result<String, io::Error> {
         info!("Loading config for {}", repo_name);
-        let contents =
-            fs::read_to_string(format!("/etc/zlorbrs/configs/{}", repo_name)).unwrap_or_default();
-        println!("Found contents: {:#?}", contents);
+        let mut contents = fs::read_to_string(format!(
+            "{}/.config/zlorbrs/configs/{}",
+            std::env::home_dir().unwrap().to_str().unwrap(),
+            repo_name
+        ));
+        if contents.is_err() {
+            info!("Theres no config so we need to create one");
+            contents = Ok(Self::save(repo_name));
+        }
+        info!("Found contents: {:#?}", contents);
+        contents
     }
 
-    fn save(repo_name: String) {
-        unimplemented!();
+    pub fn save(repo_name: String) -> String {
+        info!("Generating configuration file. System assumes Bun build script");
+        let directory_path = format!(
+            "{}/.config/zlorbrs/configs/{}",
+            std::env::home_dir().unwrap().to_str().unwrap(),
+            repo_name
+        );
+        let file_path = format!("{directory_path}/config.json");
+
+        // first create directory
+        match std::fs::create_dir_all(directory_path.clone()) {
+            Ok(_) => {
+                println!("Created config directory at: {directory_path}")
+            }
+            Err(e) => panic!("{e}"),
+        };
+
+        // then write file
+        let data = serde_json::to_string(&Config::new(repo_name)).unwrap();
+        let raw = data.clone();
+        match std::fs::write(file_path.clone(), raw) {
+            Ok(_) => {
+                println!("Created configuration file at: {file_path}")
+            }
+            Err(e) => panic!("{e}"),
+        };
+        data
     }
 }
